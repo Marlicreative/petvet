d388ad776c99c7caeece822743bdfea9bf75a67e
marlicreative <li>
2017-05-30 10:07:25 -0500

    Actualizacion perfil, editar perfil, localizacion

---
+++ app/src/main/java/com/margot/petvet/Perfil.java
@@ -0,0 +1,17 @@
+package com.margot.petvet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Margot on 26/05/17.
+ */
+
+public class Perfil extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pefil);
+    }
+}
